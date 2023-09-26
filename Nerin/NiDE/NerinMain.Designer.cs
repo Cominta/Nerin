1185562f2991c352fe45df5c347eaa0c0aade078
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-09-26 20:06:46 +0300

    NiDE new design + fix

--- Nerin/NiDE/NerinMain.Designer.cs
+++ Nerin/NiDE/NerinMain.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Nerin.NerinIDE
+﻿using System.Drawing;
+
+namespace Nerin.NerinIDE
 {
     partial class NideMain
     {
@@ -28,10 +30,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // NideMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "NideMain";
+            this.Text = "NiDE";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Nerin/NiDE/NerinMain.cs
+++ Nerin/NiDE/NerinMain.cs
@@ -14,7 +14,7 @@ namespace Nerin.NerinIDE
 {
     public partial class NideMain : Form
     {
-        private TextBox Console;
+        private TextBox MainWindow;
         private Button Compile;
         private string _inputText;
         private string _result;
@@ -29,28 +29,47 @@ namespace Nerin.NerinIDE
         {
             InitializeComponent();
 
-            this.Text = "NiDE";
-
-            Console = new TextBox();
-            Console.Multiline = true;
-            Console.Dock = DockStyle.Fill;
-            Console.ScrollBars = ScrollBars.Both;
-            Console.TextChanged += Console_TextChanged;
-            this.Controls.Add(Console);
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            this.Controls.Add(panel);
 
             Compile = new Button();
             Compile.Text = "Compile";
-            Compile.Dock = DockStyle.Top;
-            Compile.Click += ShowTextButton_Click;
-            this.Controls.Add(Compile);
+            Compile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Compile.Margin = new Padding(10);
+            Compile.Location = new Point(panel.ClientSize.Width - Compile.Width - 50, 10); // Button position
+            Compile.Click += Compile_Click;
+
+            //Add events
+            Compile.MouseEnter += Compile_MouseEnter;
+            Compile.MouseLeave += Compile_MouseLeave;
+
+            panel.BorderStyle = BorderStyle.FixedSingle;
+            panel.Controls.Add(Compile);
+            panel.BackColor = Color.FromArgb(38, 38, 38); //Panel color
+
+            //Button colors
+            Compile.BackColor = Color.FromArgb(67, 67, 67);
+            Compile.ForeColor = Color.FromArgb(255, 255, 255);
+
+            //Main window - only for text
+            MainWindow = new TextBox();
+            MainWindow.Multiline = true;
+            MainWindow.Dock = DockStyle.Bottom;
+            MainWindow.ScrollBars = ScrollBars.Both;
+            MainWindow.TextChanged += Console_TextChanged;
+            MainWindow.BorderStyle = BorderStyle.FixedSingle;
+
+            //Main window colors
+            MainWindow.BackColor = Color.FromArgb(67, 67, 67);
+            MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
+
+            MainWindow.Height = ClientSize.Height - 40;
+
+            panel.Controls.Add(MainWindow);
 
             this.KeyPreview = true;
             this.KeyDown += NideMain_KeyDown;
-        }
-
-        private void ShowTextButton_Click(object sender, EventArgs e)
-        {
-            ShowConsole();
         }
 
         // Ctrl + F5 checkout
@@ -62,41 +81,68 @@ namespace Nerin.NerinIDE
             }
         }
 
+        //Button checkout
+        private void Compile_Click(object sender, EventArgs e)
+        {
+            ShowConsole();
+        }
+
         private void ShowConsole()
         {
-            string currentStr = GetText();
+            string[] lines = GetText().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder resultBuilder = new StringBuilder();
+
             Parser parser = new Parser();
 
-            //Show result in console
-            Expr result = null;
-            parser.SetText(currentStr);
-            result = parser.Parse();
-            Evaulator evaulator = new Evaulator(result);
-            _result = evaulator.Evaluate().ToString();
+            foreach (string line in lines)
+            {
+                // Show result in console for each line
+                Expr result = null;
+                parser.SetText(line);
+                result = parser.Parse();
+                Evaulator evaulator = new Evaulator(result);
+                string lineResult = evaulator.Evaluate().ToString();
 
-            //Console settings
-            Form textDisplayForm = new Form();
-            textDisplayForm.Text = "Console";
-            textDisplayForm.Size = new System.Drawing.Size(700, 400);
+                resultBuilder.AppendLine(lineResult);
+            }
 
-            TextBox displayTextBox = new TextBox();
-            displayTextBox.Multiline = true;
-            displayTextBox.Dock = DockStyle.Fill;
-            displayTextBox.ScrollBars = ScrollBars.Both;
+            // Console settings
+            Form ConsoleWindow = new Form();
+            ConsoleWindow.Text = "Console";
+            ConsoleWindow.Size = new System.Drawing.Size(700, 400);
 
-            displayTextBox.ReadOnly = true;
+            TextBox console = new TextBox();
+            console.Multiline = true;
+            console.Dock = DockStyle.Fill;
+            console.ScrollBars = ScrollBars.Both;
 
-            displayTextBox.Text = _result;
-            
-            textDisplayForm.Controls.Add(displayTextBox);
+            console.BackColor = Color.Black;
+            console.ForeColor = Color.White;
+            console.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
-            textDisplayForm.ShowDialog();
+            console.ReadOnly = true;
+
+            console.Text = resultBuilder.ToString();
+
+            ConsoleWindow.Controls.Add(console);
+
+            ConsoleWindow.ShowDialog();
+        }
+
+        private void Compile_MouseEnter(object sender, EventArgs e)
+        {
+            Compile.BackColor = Color.FromArgb(100, 100, 100);
+        }
+
+        private void Compile_MouseLeave(object sender, EventArgs e)
+        {
+            Compile.BackColor = Color.FromArgb(67, 67, 67); // start color
         }
 
         // Save text to _inputText
         private void Console_TextChanged(object sender, EventArgs e)
         {
-            _inputText = Console.Text;
+            _inputText = MainWindow.Text;
         }
 
         public void Start()
