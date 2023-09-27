7b078c3710285fe1694eb0e0803967706640c137
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-09-27 20:14:42 +0300

    Сode optimization

--- Nerin/NiDE/NerinMain.Designer.cs
+++ Nerin/NiDE/NerinMain.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Nerin.NerinIDE
 {
@@ -43,6 +44,48 @@ namespace Nerin.NerinIDE
             this.Name = "NideMain";
             this.Text = "NiDE";
             this.ResumeLayout(false);
+
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            this.Controls.Add(panel);
+
+            Compile = new Button();
+            Compile.Text = "Compile";
+            Compile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Compile.Margin = new Padding(10);
+            Compile.Location = new Point(panel.ClientSize.Width - Compile.Width - 50, 10); // Button position
+            Compile.Click += Compile_Click;
+
+            //Add events
+            Compile.MouseEnter += Compile_MouseEnter;
+            Compile.MouseLeave += Compile_MouseLeave;
+
+            panel.BorderStyle = BorderStyle.FixedSingle;
+            panel.Controls.Add(Compile);
+            panel.BackColor = Color.FromArgb(38, 38, 38); //Panel color
+
+            //Button colors
+            Compile.BackColor = Color.FromArgb(67, 67, 67);
+            Compile.ForeColor = Color.FromArgb(255, 255, 255);
+
+            //Main window - only for text
+            MainWindow = new TextBox();
+            MainWindow.Multiline = true;
+            MainWindow.Dock = DockStyle.Bottom;
+            MainWindow.ScrollBars = ScrollBars.Both;
+            MainWindow.TextChanged += MainWindow_TextChanged;
+            MainWindow.BorderStyle = BorderStyle.FixedSingle;
+
+            //Main window colors
+            MainWindow.BackColor = Color.FromArgb(67, 67, 67);
+            MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
+
+            MainWindow.Height = ClientSize.Height - 40;
+
+            panel.Controls.Add(MainWindow);
+
+            this.KeyPreview = true;
+            this.KeyDown += NideMain_KeyDown;
 
         }
 
---
+++ Nerin/NiDE/NideConsole.cs
@@ -0,0 +1,18 @@
+﻿using Nerin.Analyzers.Items;
+using Nerin.Analyzers;
+using Nerin.NerinIDE;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nerin.NiDE
+{
+    public class NideConsole : NideMain
+    {
+        
+    }
+}
