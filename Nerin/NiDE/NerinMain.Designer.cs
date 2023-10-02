19bc6b5c7e89d943d7d16d2c7c6f19cf50fe4bbf
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-10-03 00:07:50 +0300

    Nide fix

--- Nerin/NiDE/NerinMain.Designer.cs
+++ Nerin/NiDE/NerinMain.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Nerin.NerinIDE
@@ -31,6 +32,7 @@ namespace Nerin.NerinIDE
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NideMain));
             this.SuspendLayout();
             // 
             // NideMain
@@ -40,53 +42,36 @@ namespace Nerin.NerinIDE
             this.ClientSize = new System.Drawing.Size(1067, 588);
             this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HelpButton = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "NideMain";
             this.Text = "NiDE";
             this.ResumeLayout(false);
 
-            Panel panel = new Panel();
-            panel.Dock = DockStyle.Fill;
-            this.Controls.Add(panel);
+        }
 
-            Compile = new Button();
-            Compile.Text = "Compile";
-            Compile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            Compile.Margin = new Padding(10);
-            Compile.Location = new Point(panel.ClientSize.Width - Compile.Width - 50, 10); // Button position
-            Compile.Click += Compile_Click;
+        private void SetConsole(StringBuilder resultBuilder)
+        {
+            Form ConsoleWindow = new Form();
+            ConsoleWindow.Text = "Console";
+            ConsoleWindow.Size = new System.Drawing.Size(700, 400);
 
-            //Add events
-            Compile.MouseEnter += Compile_MouseEnter;
-            Compile.MouseLeave += Compile_MouseLeave;
+            TextBox console = new TextBox();
+            console.Multiline = true;
+            console.Dock = DockStyle.Fill;
+            console.ScrollBars = ScrollBars.Both;
 
-            panel.BorderStyle = BorderStyle.FixedSingle;
-            panel.Controls.Add(Compile);
-            panel.BackColor = Color.FromArgb(38, 38, 38); //Panel color
+            console.BackColor = Color.Black;
+            console.ForeColor = Color.White;
+            console.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
-            //Button colors
-            Compile.BackColor = Color.FromArgb(67, 67, 67);
-            Compile.ForeColor = Color.FromArgb(255, 255, 255);
+            console.ReadOnly = true;
 
-            //Main window - only for text
-            MainWindow = new TextBox();
-            MainWindow.Multiline = true;
-            MainWindow.Dock = DockStyle.Bottom;
-            MainWindow.ScrollBars = ScrollBars.Both;
-            MainWindow.TextChanged += MainWindow_TextChanged;
-            MainWindow.BorderStyle = BorderStyle.FixedSingle;
+            console.Text = resultBuilder.ToString();
 
-            //Main window colors
-            MainWindow.BackColor = Color.FromArgb(67, 67, 67);
-            MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
+            ConsoleWindow.Controls.Add(console);
 
-            MainWindow.Height = ClientSize.Height - 40;
-
-            panel.Controls.Add(MainWindow);
-
-            this.KeyPreview = true;
-            this.KeyDown += NideMain_KeyDown;
-
+            ConsoleWindow.ShowDialog();
         }
 
         #endregion
--- Nerin/NiDE/NerinMain.cs
+++ Nerin/NiDE/NerinMain.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NerinLib;
 
 namespace Nerin.NerinIDE
 {
@@ -17,7 +18,7 @@ namespace Nerin.NerinIDE
         private TextBox MainWindow;
         private Button Compile;
         private string _inputText;
-        private string _result;
+        private bool error = false;
 
         public string GetText()
         {
@@ -28,67 +29,75 @@ namespace Nerin.NerinIDE
         public NideMain()
         {
             InitializeComponent();
+
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Fill;
+            this.Controls.Add(panel);
+
+            Compile = new Button();
+            Compile.Text = "Compile";
+            Compile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Compile.Margin = new Padding(10);
+            Compile.Location = new Point(panel.ClientSize.Width - Compile.Width - 50, 10); // Button position
+            Compile.Click += Compile_Click;
+
+            //Add events
+            Compile.MouseEnter += Compile_MouseEnter;
+            Compile.MouseLeave += Compile_MouseLeave;
+
+            panel.BorderStyle = BorderStyle.FixedSingle;
+            panel.Controls.Add(Compile);
+            panel.BackColor = Color.FromArgb(38, 38, 38); //Panel color
+
+            //Button colors
+            Compile.BackColor = Color.FromArgb(67, 67, 67);
+            Compile.ForeColor = Color.FromArgb(255, 255, 255);
+
+            //Main window - only for text
+            MainWindow = new TextBox();
+            MainWindow.Multiline = true;
+            MainWindow.Dock = DockStyle.Bottom;
+            MainWindow.ScrollBars = ScrollBars.Both;
+            MainWindow.TextChanged += Console_TextChanged;
+            MainWindow.TextChanged += MainWindow_TextChanged;
+            MainWindow.BorderStyle = BorderStyle.FixedSingle;
+
+            //Main window colors
+            MainWindow.BackColor = Color.FromArgb(67, 67, 67);
+            MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
+
+            MainWindow.Height = ClientSize.Height - 40;
+
+            panel.Controls.Add(MainWindow);
+
+            this.KeyPreview = true;
+            this.KeyDown += NideMain_KeyDown;
         }
 
         // Ctrl + F5 checkout
         private void NideMain_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Control && e.KeyCode == Keys.F5)
+            if (e.Control && e.KeyCode == Keys.F5 && !error)
             {
-                try
-                {
-                    ShowConsole();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("The program cannot be compiled because the code contains errors", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    MainWindow.ForeColor = Color.Red;
-                }
+                ShowConsole();
+            }
+            else if (e.Control && e.KeyCode == Keys.F5 && error)
+            {
+                MessageBox.Show("The program cannot be compiled because the code contains errors", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         //Button checkout
         private void Compile_Click(object sender, EventArgs e)
         {
-            try
-            {
-                ShowConsole();
-            }
-            catch (Exception ex)
+            if (error)
             {
                 MessageBox.Show("The program cannot be compiled because the code contains errors", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                MainWindow.ForeColor = Color.Red;
+
             }
-        }
-
-        private void MainWindow_TextChanged(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(MainWindow.Text))
+            else
             {
-                MainWindow.ForeColor = Color.White;
-                return;
-            }
-
-            try
-            {
-                string[] lines = MainWindow.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-                Parser parser = new Parser();
-
-                foreach (string line in lines)
-                {
-                    Expr result = null;
-                    parser.SetText(line);
-                    result = parser.Parse();
-                    //Evaulator evaulator = new Evaulator(result);
-
-                    //string lineResult = evaulator.Evaluate().ToString();
-                }
-
-                MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
-            }
-            catch (Exception ex)
-            {
-                MainWindow.ForeColor = Color.Red;
+                ShowConsole();
             }
         }
 
@@ -97,58 +106,20 @@ namespace Nerin.NerinIDE
             string[] lines = GetText().Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             StringBuilder resultBuilder = new StringBuilder();
 
-            Parser parser = new Parser();
+            Dictionary<string, object> variables = new Dictionary<string, object>();
 
-            if (string.IsNullOrWhiteSpace(GetText()))
+            foreach (string line in lines)
             {
-                MainWindow.ForeColor = Color.White;
-                return;
+                // Show result in console for each line
+                Compilation compilation = new Compilation(line);
+                EvaluationResult resultBound = compilation.EvaluateResult(variables);
+
+                string lineResult = resultBound.Value.ToString();
+
+                resultBuilder.AppendLine(lineResult);
             }
 
-            try
-            {
-                foreach (string line in lines)
-                {
-                    // Show result in console for each line
-                    Expr result = null;
-                    parser.SetText(line);
-                    result = parser.Parse();
-                    //Evaulator evaulator = new Evaulator(result);
-
-                    //string lineResult = evaulator.Evaluate().ToString();
-
-                    //resultBuilder.AppendLine(lineResult);
-                }
-            }
-            catch (Exception ex)
-            {
-                MainWindow.ForeColor = Color.Red;
-                return;
-            }
-
-            MainWindow.ForeColor = Color.White;
-
-            // Console settings
-            Form ConsoleWindow = new Form();
-            ConsoleWindow.Text = "Console";
-            ConsoleWindow.Size = new System.Drawing.Size(700, 400);
-
-            TextBox console = new TextBox();
-            console.Multiline = true;
-            console.Dock = DockStyle.Fill;
-            console.ScrollBars = ScrollBars.Both;
-
-            console.BackColor = Color.Black;
-            console.ForeColor = Color.White;
-            console.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-
-            console.ReadOnly = true;
-
-            console.Text = resultBuilder.ToString();
-
-            ConsoleWindow.Controls.Add(console);
-
-            ConsoleWindow.ShowDialog();
+            SetConsole(resultBuilder);
         }
 
         private void Compile_MouseEnter(object sender, EventArgs e)
@@ -165,6 +136,40 @@ namespace Nerin.NerinIDE
         private void Console_TextChanged(object sender, EventArgs e)
         {
             _inputText = MainWindow.Text;
+        }
+
+        //Text color change
+        private void MainWindow_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(MainWindow.Text))
+            {
+                MainWindow.ForeColor = Color.White;
+                error = false;
+                return;
+            }
+
+            try
+            {
+                string[] lines = MainWindow.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                Parser parser = new Parser();
+                Dictionary<string, object> variables = new Dictionary<string, object>();
+
+                foreach (string line in lines)
+                {
+                    Compilation compilation = new Compilation(line);
+                    EvaluationResult resultBound = compilation.EvaluateResult(variables);
+
+                    string lineResult = resultBound.Value.ToString();
+                }
+
+                MainWindow.ForeColor = Color.FromArgb(255, 255, 255);
+                error = false;
+            }
+            catch (Exception ex)
+            {
+                MainWindow.ForeColor = Color.Red;
+                error = true;
+            }
         }
 
         public void Start()
