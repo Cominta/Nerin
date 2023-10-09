46fe6ba428d72da1c20dd2793f2d3e22787e1ed0
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-10-09 15:43:07 +0300

    work in progress

--- Nerin/NiDE/NerinMain.Designer.cs
+++ Nerin/NiDE/NerinMain.Designer.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -42,11 +43,11 @@ namespace Nerin.NerinIDE
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.ClientSize = new System.Drawing.Size(966, 588);
             this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "NideMain";
             this.Text = "NiDE";
             this.ResumeLayout(false);
@@ -55,6 +56,7 @@ namespace Nerin.NerinIDE
 
         private void SetConsole(StringBuilder resultBuilder)
         {
+            //To receive data from NideMain class 
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NideMain));
 
             Form ConsoleWindow = new Form();
@@ -80,7 +82,7 @@ namespace Nerin.NerinIDE
             ConsoleWindow.ShowDialog();
         }
 
-        private void ShowSettingsDialog()
+        private void ShowSettings()
         {
             Form Settings = new Form();
             Settings.Text = "Settings";
@@ -129,7 +131,7 @@ namespace Nerin.NerinIDE
             Apply.Location = new Point(100, 150);
             Apply.Click += (s, e) =>
             {
-                if (float.TryParse(Font.Text, out float fontSize))
+                if (float.TryParse(Font.Value.ToString(), out float fontSize))
                 {
                     MainWindow.Font = new Font(MainWindow.Font.FontFamily, fontSize);
                 }
@@ -138,6 +140,9 @@ namespace Nerin.NerinIDE
                 this.Height = (int)HeightNumericUpDown.Value;
                 Settings.Close();
             };
+
+            Apply.MouseEnter += Apply_MouseEnter;
+            Apply.MouseLeave += Apply_MouseLeave;
 
             Settings.Controls.Add(FontLabel);
             Settings.Controls.Add(Font);
@@ -148,6 +153,25 @@ namespace Nerin.NerinIDE
             Settings.Controls.Add(Apply);
 
             Settings.ShowDialog();
+        }
+
+        public delegate void EventHandler(object sender, EventArgs e);
+
+
+        private void Apply_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Button applyButton)
+            {
+                applyButton.BackColor = Color.FromArgb(100, 100, 100);
+            }
+        }
+
+        private void Apply_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Button applyButton)
+            {
+                applyButton.BackColor = Color.Gray;
+            }
         }
 
         #endregion
--- Nerin/NiDE/NerinMain.cs
+++ Nerin/NiDE/NerinMain.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using NerinLib;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Diagnostics.Tracing;
 
 namespace Nerin.NerinIDE
 {
@@ -33,12 +34,12 @@ namespace Nerin.NerinIDE
         private void InitializeLayout()
         {
             TableLayoutPanel mainTable = new TableLayoutPanel();
-            mainTable.Dock = DockStyle.Fill;
             this.FormBorderStyle = FormBorderStyle.Sizable;
-            this.Padding = new Padding(0);
             this.Controls.Add(mainTable);
-
-            mainTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 40));
+            mainTable.Dock = DockStyle.Fill;
+            mainTable.CellBorderStyle = TableLayoutPanelCellBorderStyle.None;
+            
+            mainTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
             Panel topPanel = new Panel();
             topPanel.Dock = DockStyle.Fill;
             mainTable.Controls.Add(topPanel, 0, 0);
@@ -48,30 +49,48 @@ namespace Nerin.NerinIDE
             mainTextBox.Multiline = true;
             mainTextBox.ScrollBars = ScrollBars.Both;
             mainTextBox.Dock = DockStyle.Fill;
+            mainTextBox.BorderStyle = BorderStyle.None;
             mainTable.Controls.Add(mainTextBox, 0, 1);
 
             MainWindow = mainTextBox;
 
-            Compile = CreateButton("Compile", AnchorStyles.Top | AnchorStyles.Right, topPanel.Width / 2, 5);
-            Settings = CreateButton("Settings", AnchorStyles.Top | AnchorStyles.Right, topPanel.Width / 10, 5);
+            Compile = CreateButton("Compile", null, Color.White, Color.FromArgb(67, 67, 67), AnchorStyles.Top | AnchorStyles.Right, topPanel.Width - 100, 10);
+            Settings = CreateButton("Settings", "asd", Color.White, Color.FromArgb(67, 67, 67), AnchorStyles.Top | AnchorStyles.Right, topPanel.Width - 210, 10);
             topPanel.Controls.Add(Compile);
             topPanel.Controls.Add(Settings);
 
 
-            topPanel.BorderStyle = BorderStyle.FixedSingle;
+            topPanel.BorderStyle = BorderStyle.None;
             topPanel.BackColor = Color.FromArgb(38, 38, 38);
             mainTextBox.BackColor = Color.FromArgb(67, 67, 67);
             mainTextBox.ForeColor = Color.FromArgb(255, 255, 255);
         }
 
-        private Button CreateButton(string text, AnchorStyles anchor, int x, int y)
+        private Button CreateButton(string text, string icon, Color fore_color, Color back_color, AnchorStyles anchor, int x, int y)
         {
             Button button = new Button();
-            button.Text = text;
-            button.Location = new Point(x, y);
-            button.Anchor = anchor;
-            button.Margin = new Padding(10);
-            return button;
+
+            if(icon == null)
+            {
+                button.Text = text;
+                button.Location = new Point(x, y);
+                button.Anchor = anchor;
+                button.Margin = new Padding(10);
+                button.ForeColor = fore_color;
+                button.BackColor = back_color;
+                return button;
+            }
+            else
+            {
+                button.Image = Properties.Resources.settings;
+                button.ImageAlign = ContentAlignment.MiddleLeft;
+                button.Location = new Point(x, y);
+                button.Anchor = anchor;
+                button.Margin = new Padding(10);
+                button.ForeColor = fore_color;
+                button.BackColor = back_color;
+                return button;
+            }
         }
 
         private void InitializeEventHandlers()
@@ -81,6 +100,9 @@ namespace Nerin.NerinIDE
             Compile.MouseLeave += Compile_MouseLeave;
 
             Settings.Click += SettingsButton_Click;
+            Settings.MouseEnter += Settings_MouseEnter;
+            Settings.MouseLeave += Settings_MouseLeave;
+
             MainWindow.TextChanged += Console_TextChanged;
             MainWindow.TextChanged += MainWindow_TextChanged;
 
@@ -114,7 +136,7 @@ namespace Nerin.NerinIDE
 
         private void SettingsButton_Click(object sender, EventArgs e)
         {
-            ShowSettingsDialog();
+            ShowSettings();
         }
 
         private void ShowConsole()
@@ -145,6 +167,16 @@ namespace Nerin.NerinIDE
         private void Compile_MouseLeave(object sender, EventArgs e)
         {
             Compile.BackColor = Color.FromArgb(67, 67, 67); // start color
+        }
+
+        private void Settings_MouseEnter(object sender, EventArgs e)
+        {
+            Settings.BackColor = Color.FromArgb(100, 100, 100);
+        }
+
+        private void Settings_MouseLeave(object sender, EventArgs e)
+        {
+            Settings.BackColor = Color.FromArgb(67, 67, 67); // start color
         }
 
         private void Console_TextChanged(object sender, EventArgs e)
